941528571c898083e1ea31fc65541db0cdcda12e
Nidhi Kulkarni <nkulkarni@mux.com>
2020-03-09 10:16:34 -0700

    Initial Commit

---
+++ TikTok/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TikTok
+//
+//  Created by Nidhi Kulkarni on 3/9/20.
+//  Copyright © 2020 Nidhi Kulkarni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
