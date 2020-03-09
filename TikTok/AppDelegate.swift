3453c3ae10c1ddb823ab17cbe9168ef6e3c74047
Nidhi Kulkarni <nkulkarni@mux.com>
2020-03-09 13:34:00 -0700

    initial commit

--- TikTok/AppDelegate.swift
+++ TikTok/AppDelegate.swift
@@ -8,10 +8,11 @@
 
 import UIKit
 
+@available(iOS 13.0, *)
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
@@ -20,6 +21,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     // MARK: UISceneSession Lifecycle
 
+    @available(iOS 13.0, *)
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
--- TikTok/ViewController.swift
+++ TikTok/ViewController.swift
@@ -7,14 +7,78 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController  {
 
+    @IBOutlet weak var previewView: UIView!
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet var captureView: UIView!
+    var captureSession: AVCaptureSession!
+    var captureOutput: AVCaptureMovieFileOutput!
+    var videoPreviewLayer: AVCaptureVideoPreviewLayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.captureSession.stopRunning()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        // Setup camera here
+        captureSession = AVCaptureSession()
+        captureSession.sessionPreset = .medium
+        guard let rearCamera = AVCaptureDevice.default(for: AVMediaType.video),
+            let audioInput = AVCaptureDevice.default(for: AVMediaType.audio)
+            else {
+                print("Unable to access capture devices!")
+                return
+        }
+        do {
+            let cameraInput = try AVCaptureDeviceInput(device: rearCamera)
+            let audioInput = try AVCaptureDeviceInput(device: audioInput)
+            captureOutput = AVCaptureMovieFileOutput()
+            if captureSession.canAddInput(cameraInput) && captureSession.canAddInput(audioInput) && captureSession.canAddOutput(captureOutput) {
+                captureSession.addInput(cameraInput)
+                captureSession.addInput(audioInput)
+                captureSession.addOutput(captureOutput)
+                setupLivePreview()
+            }
+        }
+        catch let error  {
+            print("Error Unable to initialize inputs:  \(error.localizedDescription)")
+        }
+    }
+    
+    func setupLivePreview() {
+        
+        videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        videoPreviewLayer.backgroundColor = UIColor.green.cgColor
+        videoPreviewLayer.videoGravity = .resizeAspectFill
+        videoPreviewLayer.connection?.videoOrientation = .portrait
+        previewView.layer.addSublayer(videoPreviewLayer)
+        
+        DispatchQueue.global(qos: .userInitiated).async { [weak self] in
+            guard let self = self else { return }
+            self.captureSession?.startRunning()
+            DispatchQueue.main.async { [weak self] in
+                guard let self = self else { return }
+                self.videoPreviewLayer.frame = self.previewView.bounds
+            }
+        }
     }
 
-
+    @IBAction func tappedRecord(_ sender: Any) {
+        // TODO
+        print("Record tapped")
+    }
+    
+    @IBAction func tappedFlipCamera(_ sender: Any) {
+        // TODO: flip the camera
+        print("Flip camera tapped")
+    }
 }
-
