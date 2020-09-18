7ca5ab06acb4f68f304ea935803600e925265720
Nidhi Kulkarni <nkulkarni@mux.com>
2020-09-18 05:31:04 -0400

    remove dead code calling Mux API from the client

--- TikTok/MuxApiClient.swift
+++ TikTok/MuxApiClient.swift
@@ -12,36 +12,6 @@ import Parse
 
 class MuxApiClient: NSObject {
     
-    func createUploadUrlForAsset(completion: @escaping (URL?, Error?) -> Void) {
-        let parameters = [
-            "new_asset_settings":[
-                "playback_policies":["public"]
-            ]
-        ]
-        let username = "9ffe0382-a562-4c1c-abcb-848eccd00505"
-        let password = "+tynuWL/H2/QNcVbSy/MkeFfEOjY6P0/IiYaI8n3ZqhGeyjZQYlC+U97VoySaGDS7iJNmPEPK0Z"
-        let credentialData = "\(username):\(password)".data(using: .utf8)!
-        let base64Credentials = credentialData.base64EncodedString()
-        
-        let headers = ["Authorization": "Basic \(base64Credentials)"]
-        
-        AF.request("https://api.mux.com/video/v1/uploads", method: .post, parameters: parameters, encoder: JSONParameterEncoder.default, headers: HTTPHeaders(headers))
-            .responseJSON { response in
-                switch response.result {
-                case .success(let json):
-                    let result = json as! Dictionary<String, Any>
-                    guard let data = result["data"] as? Dictionary<String, Any>,
-                        let location = data["url"] as? String else {
-                            completion(nil, NSError(domain: "com.nidhi.tiktok", code: 400, userInfo: nil))
-                            return
-                    }
-                    completion(URL(string:  location), nil)
-                case let .failure(error):
-                    completion(nil, error)
-                }
-        }
-    }
-    
     func uploadFileToParse(fileURL: URL) {
         do {
             let file = try PFFileObject(name: "myvideo.mp4", contentsAtPath: fileURL.path)
@@ -58,18 +28,6 @@ class MuxApiClient: NSObject {
         } catch (let error) {
             print(error)
         }
-    }
-    
-    func uploadFileToMux(fileURL: URL) {
-        createUploadUrlForAsset(completion: {(url, error) in
-            if (error == nil) {
-                AF.upload(fileURL, to: url!, method: .put).responseJSON { response in
-                    debugPrint(response)
-                }
-            } else {
-                print("Could not create direct upload url: \(String(describing: error))")
-            }
-        })
     }
     
     func uploadVideo(fileURL: URL) {
